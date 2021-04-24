e16ba90128c748bd3627e6eda83d8d246c7d71a1
Lucile <developer.lvt@gmail.com>
2021-04-23 21:29:06 -0400

    added useState hook to team details

--- src/components/right-panel/subcontainers/TabDetails.js
+++ src/components/right-panel/subcontainers/TabDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 
 import "../RightPanel.css";
 
@@ -17,12 +17,19 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function TabDetails() {
+  const [isClicked,setClicked] = useState(true)
+
+  function handleClick(){
+      isClicked ? setClicked(false) : setClicked(true)
+      console.log(isClicked)
+  }
+
   return (
     <div className="tabDetails">
       <p><FontAwesomeIcon icon={faFileAlt}/> Description</p>
       <p><FontAwesomeIcon icon={faCircle}/> Statut</p>
-      <p><FontAwesomeIcon icon={faUsers}/> Équipe <span>Gabrielle Chevalier</span></p>
-      <TeamDetails />
+      <p onClick={handleClick}><FontAwesomeIcon icon={faUsers}/> Équipe <span>Gabrielle Chevalier</span></p>
+      {isClicked ? <TeamDetails /> :""}
       <p><FontAwesomeIcon icon={faMoneyCheck}/> Honoraires</p>
       <p><FontAwesomeIcon icon={faClock}/> Durée</p>
       <p><FontAwesomeIcon icon={faCalendarAlt}/> Date butoir</p>
