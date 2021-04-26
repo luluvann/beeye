75d2f1de8525a263033a0025ef3ae73acf8140dd
Lucile <developer.lvt@gmail.com>
2021-04-26 13:33:27 -0400

    moved the graph component

--- src/components/right-panel/subcontainers/team-details/TeamTable.js
+++ src/components/right-panel/subcontainers/team-details/TeamTable.js
@@ -4,22 +4,20 @@ import "./TeamTable.css";
 
 import Avatar from "../../../Avatar";
 
-import navAvatar from "../../../../assets/navAvatar.jpg";
 import avatarEmployee1 from "../../../../assets/avatarEmployee1.jpg";
 import avatarEmployee2 from "../../../../assets/avatarEmployee2.jpg";
 import avatarEmployee3 from "../../../../assets/avatarEmployee3.jpg";
 import avatarEmployee4 from "../../../../assets/avatarEmployee4.jpg";
 
-import Planner from "./planner/Planner"
+import Planner from "./planner/Planner";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 function TeamTable() {
   const TeamMembers = [
     {
-      name: "Gabrielle Chevalier",
-      poste: "Assistant Comptable",
-      img: navAvatar,
-    },
-    {
+      id: "02",
       name: "Marc Chapuis",
       poste: "Assistant Comptable",
       img: avatarEmployee1,
@@ -30,30 +28,31 @@ function TeamTable() {
       img: avatarEmployee3,
     },
     {
+      id: "03",
       name: "Mathieu Ancel",
       poste: "Assistant Comptable",
       img: avatarEmployee2,
     },
     {
+      id: "04",
       name: "Marie-Noëlle Auvray",
       poste: "Assistant Comptable",
       img: avatarEmployee4,
     },
   ];
- 
+
   const TeamMemberRows = TeamMembers.map((teamMember, index) => {
     return (
       <>
-      <tr key={index} className="teamMemberContainer">
+        <tr key={index} className="teamMemberContainer">
           <td className="avatarEmployee">
             <Avatar src={teamMember.img} />
           </td>
-        <div className="NamePosition">
-          <td>{teamMember.name}</td>
-          <td className="position">{teamMember.poste}</td>
-        </div>
-      </tr>
-      <Planner />
+          <div className="NamePosition">
+            <td>{teamMember.name}</td>
+            <td className="position">{teamMember.poste}</td>
+          </div>
+        </tr>
       </>
     );
   });
@@ -63,6 +62,16 @@ function TeamTable() {
       <tr>
         <th>Nom</th>
       </tr>
+      <tr className="teamMemberContainer">
+        <td className="checkCircle">
+          <FontAwesomeIcon icon={faCheckCircle} style={{borderRadius:"30px",backgroundColor:"orange",color:"#fff3e2", fontSize:"55px"}} />
+        </td>
+        <div className="NamePosition">
+          <td>Gabrielle Chevalier</td>
+          <td className="position">Assistant comptable</td>
+        </div>
+      </tr>
+      <Planner />
       {TeamMemberRows}
     </table>
   );
