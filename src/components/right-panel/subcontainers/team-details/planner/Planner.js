0aab88433642665b9b293fbbe58e9ca1aeb5481a
Lucile <developer.lvt@gmail.com>
2021-04-25 22:35:40 -0400

    added graph legend

--- src/components/right-panel/subcontainers/team-details/planner/Planner.js
+++ src/components/right-panel/subcontainers/team-details/planner/Planner.js
@@ -1,24 +1,36 @@
 import React from "react";
-import "./Planner.css"
-import GraphBar from "./Graphbar"
-import Dates from "./Dates"
+import "./Planner.css";
+import GraphBar from "./Graphbar";
+import Dates from "./Dates";
 import { NavItem } from "react-bootstrap";
 
-function Planner(){
+function Planner() {
+  const graphBars = Dates.map((item) => {
+    return <GraphBar date={item.date} completion={item.completion} />;
+  });
 
-    const graphBars = Dates.map(item => {
-        return <GraphBar date={item.date} completion={item.completion}/>
-    })
-
-    return(
-        <div className="plannerContainer">
-            <div className="graph">
-                {graphBars}
-            </div>
-            <div className="legend">
-            </div>
+  return (
+    <div className="plannerContainer">
+      <div className="graph">{graphBars}</div>
+      <div className="legend">
+        <div className="dateRange">
+          <a href="#">
+            &#8249;
+          </a>
+          <p>11/11/2020 - 11/11/2020</p>
+          <a href="#">
+            &#8250;
+          </a>
         </div>
-    )
+        <div className="legendDetails">
+          <p>Budget restant</p>
+          <p>Disponibilité restante</p>
+          <p>Planifié sur d'autres tâches</p>
+          <p>Planifié sur cette tâche</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Planner;
