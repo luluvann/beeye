19e17847fbaf8b305c4d22ee5763bb3fef0baa73
Lucile <developer.lvt@gmail.com>
2021-04-28 13:11:40 -0400

    fixed graph padding and bar highlight

--- src/components/right-panel/subcontainers/team-details/planner/Graphbar.js
+++ src/components/right-panel/subcontainers/team-details/planner/Graphbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Planner.css";
 
-function Graphbar({properties}) {
- 
+function Graphbar({ properties }) {
   return (
     <div className="graphBarContainer">
-      <div
-        className="graphBar"
-        style={{
-          background: `linear-gradient(0deg, ${properties.barColor ? properties.barColor : "rgba(241,240,250,1)"} ${properties.completion}%,  rgba(241,240,250,1)  ${properties.completion}%)`,
-        }}
-      ></div>
-      <div className="increaseDecreaseButton">+</div>
-      <div className="increaseDecreaseButton">-</div>
+      <div className="graphBarHighlight">
+        <div
+          className="graphBar"
+          style={{
+            background: `linear-gradient(0deg, ${
+              properties.barColor ? properties.barColor : "rgba(241,240,250,1)"
+            } ${properties.completion}%,  rgba(241,240,250,1)  ${
+              properties.completion
+            }%)`,
+          }}
+        ></div>
+        <div className="increaseDecreaseButton">+</div>
+        <div className="increaseDecreaseButton">-</div>
+      </div>
       <div className="graphBarDate">{properties.date}</div>
     </div>
   );
